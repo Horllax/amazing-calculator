3f1029cb4b79aae877d77b4eb4aca8a1bdf419ed
horllax <akintundeolajuwonstephen@gmail.com>
2021-09-28 13:05:32 +0100

    my first react.js project

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ function App() {
   const [result, setResult] = useState('');
   const ops = ['/', '*', '+', '-', '.']
 
-  const updateCalc = value => {
+  const updateCalc = number => {
     if(
-      ops.includes(value) && calc === '' ||
-      ops.includes(value) && ops.includes(calc.slice(-1)
+      ops.includes(number) && calc === '' ||
+      ops.includes(number) && ops.includes(calc.slice(-1)
       )
       )
       {
         return;
       }
-     setCalc(calc + value);
-     if(!ops.includes(value)){
-       setResult(eval(calc + value).toString());
+     setCalc(calc + number);
+     if(!ops.includes(number)){
+       setResult(eval(calc + number).toString());
      }
   }
 
@@ -52,7 +52,7 @@ function App() {
               </div>
               <div className = 'operator'>
                    <button onClick={()=> updateCalc('/')}>/</button>
-                   <button onClick={()=> updateCalc('*')}>*</button>
+                   <button onClick={()=> updateCalc('*')}>x</button>
                    <button onClick={()=> updateCalc('+')}>+</button>
                    <button onClick={()=> updateCalc('-')}>-</button>
 
